a5554b0bf9eebdac6458d454bfbad81b70527392
rakudayo <rakudayo@gmail.com>
2015-05-26 22:27:01 -0700

    Initial commit from skeleton

---
+++ environment.rb
@@ -0,0 +1,3 @@
+# Put code here to initialize your environment
+
+Dir[File.expand_path('lib/**/*') + ".rb"].each {|f| require f}
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+Bundler.setup :default, :test
+Bundler.require :default, :test
+
+require_relative '../environment'
+
+RSpec.configure do |config|
+  # Configure rspec here
+end
